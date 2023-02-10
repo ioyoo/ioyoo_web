3c8b2c84f4cee9f4ce8f2dd9de242b8fab61ad30
Andrés Naves <andres.naves.mauri@hp.com>
2023-02-10 17:37:51 +0100

    Added tailwind dependency

---
+++ ioyoo/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: rgb(14, 232, 163);
+}
+</style>
